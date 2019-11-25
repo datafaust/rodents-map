b146bd1cfb01d1d8c4c03d50b7caaae7c27efd26
ulysses <faustolopez110@gmail.com>
2019-11-25 14:55:06 -0500

    Updates

---
+++ precache-manifest.4d4169f3dc3f4788bb142887d5b1d901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11380da3e42a8985b57871f114762e9e",
+    "url": "/rodents-map/index.html"
+  },
+  {
+    "revision": "4d8a681743ebcb8deb6d",
+    "url": "/rodents-map/static/css/main.74486bd3.chunk.css"
+  },
+  {
+    "revision": "2300b2940402570277eb",
+    "url": "/rodents-map/static/js/2.ef715d81.chunk.js"
+  },
+  {
+    "revision": "4d8a681743ebcb8deb6d",
+    "url": "/rodents-map/static/js/main.81d818fa.chunk.js"
+  },
+  {
+    "revision": "4338cde2d5583fb48e7c",
+    "url": "/rodents-map/static/js/runtime-main.3bfc2ee8.js"
+  },
+  {
+    "revision": "de3ae7fe5af9ff59b66d30c98f54a172",
+    "url": "/rodents-map/static/media/scared3.de3ae7fe.png"
+  }
+]);
